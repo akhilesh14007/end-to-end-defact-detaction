8f89822f20ac6ed0eb6ca861b79df8246fc92950
Akhilesh <akhileshsingh1400@gmail.com>
2024-04-26 11:52:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+
+    name ='Defact_detaction',
+    version='0.0.0',
+    author='Akhilesh singh sikarwar',
+    author_eamil='akhileshsingh1407@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
